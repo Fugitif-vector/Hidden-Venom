0370dca83a1624ca9d0917e96f0b063f4b364a75
♚₣ugi☨!ɟ☠☣ <102538465+Fugitif-vector@users.noreply.github.com>
2022-10-02 17:07:16 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,4 @@
+chmod +x hvenom
+install hvenom /data/data/com.termux/files/usr/bin/hvenom
+
+echo [√] hvenom successfully installed. Enter the hvenom command from any directory to create a hidden payload.
